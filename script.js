fc2d25ed28e015479b0888f63aa943cf4a219d74
Jolson Cruz <56365930+jolsoncruz@users.noreply.github.com>
2020-09-23 01:12:23 +0800

    Error Checker for empty input

--- script.js
+++ script.js
@@ -1,47 +1,50 @@
 function calculate(){
-    document.getElementById('errorMsg').innerHTML = ""
-    var binary = document.getElementById('binary').value;
-    var bits = document.getElementById('bits').value;
-    var binaryList = binary.split("")
+    document.getElementById('errorMsg').innerHTML = "";
+    var inputBinary = document.getElementById('inputBinary').value;
+    var inputBits = document.getElementById('inputBits').value;
+    var binaryList = inputBinary.split("");
 
-    console.log(binary, bits, binaryList);
+    console.log(inputBinary, inputBits, binaryList);
 
-    var truncate = []
-    var roundUp = 0
-    var roundDown = 0
-    var ties = 0
+    var truncate = [];
+    var roundUp = 0;
+    var roundDown = 0;
+    var ties = 0;
     var isBinary = true;
-    var errorMsg = "ERR: INPUT IS NOT A BINARY"
-    var invalidLength = "ERR: INVALID # OF BITS"
+    var errorMsg1 = "ERR: INPUT IS NOT A BINARY";
+    var errorMsg2 = "ERR: INVALID # OF BITS";
+    var errorMsg3 = "ERR: ALL FIELDS ARE REQUIRED";
 
     for(i = 0; i < binaryList.length; i ++){
         if(binaryList[i] === "0" || binaryList[i] === "1" || binaryList[i] === "." || binaryList[i] === "-" || binaryList[i] === "+"){
             console.log("BINARY FOUND");
         } else {
             isBinary = false;
-            console.log(errorMsg)
+            console.log(errorMsg1);
         }
     }
 
-    if(bits <= binaryList.length && bits >= 0){
-        if(isBinary == true){
-            for(i = 0; i < bits; i ++){
-                truncate = truncate + binaryList[i];
-                if(binaryList[i] === "." || binaryList[i] === "-" || binaryList[i] === "+"){
-                    bits++;
+    if(inputBinary !== "" && inputBits !== ""){
+        if(inputBits <= binaryList.length && inputBits >= 0){
+            if(isBinary == true){
+                for(i = 0; i < inputBits; i ++){
+                    truncate = truncate + binaryList[i];
+                    if(binaryList[i] === "." || binaryList[i] === "-" || binaryList[i] === "+"){
+                        inputBits++;
+                    }
                 }
+                console.log(truncate)
+                document.getElementById('truncate').innerHTML = truncate;
+                document.getElementById('roundUp').innerHTML = roundUp;
+                document.getElementById('roundDown').innerHTML = roundDown;
+                document.getElementById('ties').innerHTML = ties;
+            } else {
+                document.getElementById('errorMsg').innerHTML = errorMsg1;
             }
-        
-            console.log(truncate)
-        
-            document.getElementById('truncate').innerHTML = truncate;
-            document.getElementById('roundUp').innerHTML = roundUp;
-            document.getElementById('roundDown').innerHTML = roundDown;
-            document.getElementById('ties').innerHTML = ties;
         } else {
-            document.getElementById('errorMsg').innerHTML = errorMsg;
+            document.getElementById('errorMsg').innerHTML = errorMsg2;
         }
     } else {
-        document.getElementById('errorMsg').innerHTML = invalidLength;
+        document.getElementById('errorMsg').innerHTML = errorMsg3;
     }
 }

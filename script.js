a9f24c2974ffe48c1da809bfd02f5ac64ee4ecc0
Jolson Cruz <56365930+jolsoncruz@users.noreply.github.com>
2020-09-24 16:29:17 +0800

    Included the Binary Sign

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.addEventListener('load', () => {
         document.getElementById('errorMsg').innerHTML = "";
         var inputBinary = document.getElementById('inputBinary').value;
         var inputBits = document.getElementById('inputBits').value;
+        var inputSign = $("input[name='inputSign']:checked").val();
         var binaryList = inputBinary.split("");
 
         console.log(inputBinary, inputBits, binaryList);
@@ -21,28 +22,52 @@ window.addEventListener('load', () => {
         //Refresh
         $('#inputBinary').removeClass('border-danger');
         $('#inputBits').removeClass('border-danger');
+        $('#inputSignBox').removeClass('border border-danger');
         document.getElementById('truncate').innerHTML = 0;
         document.getElementById('roundUp').innerHTML = 0;
         document.getElementById('roundUp').innerHTML = 0;
         document.getElementById('roundUp').innerHTML = 0;
 
-        //Binary Input First Checker
-        for(i=0; i<binaryList.length; i++){
-            if(binaryList[i] === ".") numDot++;
-            if(binaryList[i] === "+" || binaryList[i] === "-") numSign++
-            if(binaryList[i] !== "0" && binaryList[i] !== "1" && binaryList[i] !== "." && binaryList[i] !== "-" && binaryList[i] !== "+") isBinary = false;
-        };  if(numDot>1 || numSign>1) err1();
+        //Empty Checker
+        if(inputBinary===""){   
+            if(inputBits===""){
+                if(inputSign===undefined){
+                    warnBinary();
+                    warnBits();
+                    warnSign();
+                    err3();
+                } else{
+                    warnBinary();
+                    warnBits();
+                    err3();
+                }
+            } else{
+                warnBinary();
+                err3();
+            }
+        } else if(inputBits===""){
+            if(inputSign===undefined){
+                warnBits();
+                warnSign();
+                err3();
+            } else{
+                warnBits();
+                err3();
+            }
+        } else if(inputSign===undefined){
+            warnSign();
+            err3();
+        };
 
         //PRE-OPERATION
         if(isDone !== true){
-            //Empty Checker
-            if(inputBinary===""){   
-                if(inputBits===""){
-                    err5();
-                } else err3();
-            } else if(inputBits===""){
-                err4();
-            };
+            //Binary Input First Checker
+            for(i=0; i<binaryList.length; i++){
+                if(binaryList[i] === ".") numDot++;
+                if(binaryList[i] === "+" || binaryList[i] === "-") numSign++
+                if(binaryList[i] !== "0" && binaryList[i] !== "1" && binaryList[i] !== "." && binaryList[i] !== "-" && binaryList[i] !== "+") isBinary = false;
+            };  if(numDot!=1 || numSign>0) err1();
+
 
             //PRE-OPERATION CHECKERS
             if(isEmpty !== true){
@@ -70,64 +95,62 @@ window.addEventListener('load', () => {
             }
             document.getElementById('truncate').innerHTML = truncate;
             console.log("Truncate: " + truncate);
-        }
+        };
 
         //Round Up
         function arRup(){
             document.getElementById('roundUp').innerHTML = roundUp;
             console.log("Round Up: " + roundUp);
-        }
+        };
 
         //Round Down
         function arRdown(){
             document.getElementById('roundUp').innerHTML = roundDown;
             console.log("Round Down: " + roundDown);
-        }
+        };
 
         //Ties to Even
         function arTte(){
             document.getElementById('roundUp').innerHTML = ties;
             console.log("Round Ties To Even: " + ties);
-        }
+        };
 
         //ERROR MESSAGES
         //Error 1: Input is not a binary
         function err1(){
-            $('#inputBinary').addClass('border border-danger');
+            warnBinary();
             document.getElementById('errorMsg').innerHTML = "INVALID BINARY";
             isDone = true;
         };
 
         //Error 2: Invalid number of bits
         function err2(){
-            $('#inputBits').addClass('border border-danger');
+            warnBits();
             document.getElementById('errorMsg').innerHTML = "INVALID # OF BITS";
             isDone = true;
         };
 
         //Error 3: Empty binary
         function err3(){
-            $('#inputBinary').addClass('border border-danger');
-            document.getElementById('errorMsg').innerHTML = "BINARY IS REQUIRED";
+            document.getElementById('errorMsg').innerHTML = "ALL FIELDS ARE REQUIRED";
             isDone = true;
             isEmpty = true;
         };
 
-        //Error 4: Empty number of bits
-        function err4(){
-            $('#inputBits').addClass('border border-danger');
-            document.getElementById('errorMsg').innerHTML = "NUMBER OF BITS IS REQUIRED";
-            isDone = true;
-            isEmpty = true;
+        //CSS FUNCTIONS
+        //Borderize Input Binary
+        function warnBinary(){
+            $('#inputBinary').addClass('border border-danger');
         };
 
-        //Error 5: Both fields are empty
-        function err5(){
-            $('#inputBinary').addClass('border border-danger');
+        //Borderize Input Bits
+        function warnBits(){
             $('#inputBits').addClass('border border-danger');
-            document.getElementById('errorMsg').innerHTML = "BOTH FIELDS ARE REQUIRED";
-            isDone = true;
-            isEmpty = true;
+        };
+
+        //Borderize Input Sign
+        function warnSign(){
+            $('#inputSignBox').addClass('border border-danger');
         };
     });
 });

c3555a5cdbae2f9841020e5429a1f001a4518477
Jolson Cruz <56365930+jolsoncruz@users.noreply.github.com>
2020-09-25 00:43:07 +0800

    Finished Truncation

--- script.js
+++ script.js
@@ -22,110 +22,99 @@ window.addEventListener('load', () => {
         var lastNz = -1;    //last non zero index
         var dotIndex = -1;   //index of dot
         var sigNum = -1;     //number of significant
-        var sigOut = 
 
         var userBits = inputBits;
         var ctr1 = 0;
 
 
         refresh();
+        emptyChecker();
 
-        //Empty Checker
-        if(inputBinary===""){   
-            if(inputBits===""){
-                if(inputSign===undefined){
+        //PRE-OPERATION
+        if(isDone === false){
+            indexScanner();
+            if(isBinary == true){
+                //Number of Bits Chcecker
+                if(inputBits <= sigNum && inputBits > 0){                  
+                    //ACTUAL OPERATIONS
+                    arTrunc();
+                    arRup();
+                    arRdown();
+                    arTte();
+                    isDone = true;
+                } else err2();
+            } 
+        };
+
+        document.getElementById('debug').innerHTML = sigNum;
+
+        //CHECKER FUNCTIONS
+        //Empty Field Checker
+        function emptyChecker(){
+            if(inputBinary===""){   
+                if(inputBits===""){
+                    if(inputSign===undefined){
+                        warnBinary();
+                        warnBits();
+                        warnSign();
+                        err3();
+                    } else{
+                        warnBinary();
+                        warnBits();
+                        err3();
+                    }
+                } else{
                     warnBinary();
+                    err3();
+                }
+            } else if(inputBits===""){
+                if(inputSign===undefined){
                     warnBits();
                     warnSign();
                     err3();
                 } else{
-                    warnBinary();
                     warnBits();
                     err3();
                 }
-            } else{
-                warnBinary();
-                err3();
-            }
-        } else if(inputBits===""){
-            if(inputSign===undefined){
-                warnBits();
+            } else if(inputSign===undefined){
                 warnSign();
                 err3();
-            } else{
-                warnBits();
-                err3();
-            }
-        } else if(inputSign===undefined){
-            warnSign();
-            err3();
-        };
+            };
+        }
 
-        //PRE-OPERATION
-        if(isDone === false){
-            //Binary Input First Checker
+        //Index Scanner
+        function indexScanner(){
             for(i=0; i<binaryList.length; i++){
                 if(binaryList[i] === ".") numDot++;
                 if(binaryList[i] !== "0" && binaryList[i] !== "1" && binaryList[i] !== ".") isBinary = false;
+                if(binaryList[i] !== "0" && binaryList[i] !== "."){
+                    if(firstNz === -1) firstNz=i;
+                    lastNz = i;
+                }
+                else if(binaryList[i] === ".") dotIndex=i;
             };  if(numDot>1) isBinary = false;
 
-            scanIndex();
-
-            //PRE-OPERATION CHECKERS
-            if(isEmpty === false){
-                //Number of Bits Chcecker
-                if(inputBits <= sigNum && inputBits > 0){  
-                    //Binary Input Second Checker
-                    if(isBinary == true){
-                        //ACTUAL OPERATIONS
-                        arTrunc();
-                        arRup();
-                        arRdown();
-                        arTte();
-                        isDone = true;
-                    } else err1();
-                } else err2();
-            };
+            if(isBinary === true){
+                if(dotIndex !== -1){
+                    if(firstNz === -1 && lastNz === -1) sigNum = lastNz - firstNz;
+                    else if(dotIndex<firstNz) sigNum = binaryList.length - firstNz;
+                    else if(dotIndex>firstNz) sigNum = binaryList.length - 1 - firstNz;
+                }
+                else if(dotIndex === -1){
+                    if(firstNz === -1 && lastNz === -1) sigNum = 0;
+                    else sigNum = lastNz - firstNz + 1;
+                }
+                if(sigNum === 0) isBinary = false;
+                console.log("Dot Index: " + dotIndex);
+                console.log("First NZ Index: " + firstNz);
+                console.log("Last NZ Index: " + lastNz);
+                console.log("Significant Num: " + sigNum);
+            }   if(isBinary === false) err1();
         };
 
         //ROUNDING OPERATIONS
         //Truncate
-
-        function scanIndex(){
-            for(i=0; i<binaryList.length; i++){
-                if(binaryList[i] !== "0" && binaryList[i] !== "." && firstNz === -1) firstNz=i;
-                else if(binaryList[i] !== "0" && binaryList[i] !== ".") lastNz=i;
-                else if(binaryList[i] === ".") dotIndex=i;
-            }
-
-            if(dotIndex !== -1 && dotIndex<firstNz) sigNum = binaryList.length - firstNz;
-            else if(dotIndex !== -1 && dotIndex>firstNz) sigNum = binaryList.length - 1 - firstNz;
-            else if(firstNz !== -1 && lastNZ !== -1) sigNum = lastNz - firstNz + 1;
-            else sigNum = 0;
-
-            console.log("Dot Index: " + dotIndex);
-            console.log("First NZ Index: " + firstNz);
-            console.log("Last NZ Index: " + lastNz);
-            console.log("Significant Num: " + sigNum);
-        }
-
         function arTrunc(){
-<<<<<<< Updated upstream
-            for(i = 0; i < binaryList.length; i ++){
-                if(binaryList[i] !== undefined){
-                    if(binaryList[i] === ".") inputBits++;
-                    if(dotIndex !== -1){
-                        console.log("temp");
-                    } else{ 
-                        while(sigOut<sigNum){
-                            if(i>=firstNz && i<=lastNz){
-                                truncate = truncate + binaryList[i];
-                                sigOut++;
-                                i++;
-                            }
-                        }
-                    }
-=======
             for(i = 0; i < binaryList.length; i++){
                 if(binaryList[i] !== undefined){
                     if(dotIndex !== -1 && ctr1 < userBits){    //if dot exists
@@ -140,7 +129,6 @@ window.addEventListener('load', () => {
                             ctr1++;
                         };
                     };
->>>>>>> Stashed changes
                 } else err2();
             };
             if(isDone === false){
@@ -206,7 +194,7 @@ window.addEventListener('load', () => {
             $('#inputSignBox').addClass('border border-danger');
         };
 
-        //REFRESH FUNCTION
+        //Refresh Function
         function refresh(){
             $('#inputBinary').removeClass('border-danger');
             $('#inputBits').removeClass('border-danger');
